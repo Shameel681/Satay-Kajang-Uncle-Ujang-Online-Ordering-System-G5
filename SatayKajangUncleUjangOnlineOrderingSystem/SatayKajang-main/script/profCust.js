c9a4000a248601b35adf71417495ec8233bb7912
Shameel681 <shameel681@gmail.com>
2025-08-24 00:05:37 +0800

    TEST

--- SatayKajangUncleUjangOnlineOrderingSystem/SatayKajang-main/script/profCust.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const editBtn = document.getElementById("editProfileBtn");
-
-    editBtn.addEventListener("click", () => {
-        alert("Feature under development: Profile editing will be available soon!");
-        // Nanti boleh buat redirect ke profEdit.php
-    });
-});
