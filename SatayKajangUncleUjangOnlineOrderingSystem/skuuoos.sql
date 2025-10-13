153a02061e2318203db8780ad9fc974642b7f8aa
Shameel681 <shameel681@gmail.com>
2025-10-13 18:38:25 +0800

    database update

--- SatayKajangUncleUjangOnlineOrderingSystem/skuuoos.sql
+++ SatayKajangUncleUjangOnlineOrderingSystem/skuuoos.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2025 at 09:40 AM
+-- Generation Time: Oct 13, 2025 at 12:35 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -49,8 +49,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `admin_name`, `email`, `phone_no`, `address`, `password`, `is_verified`, `verify_token`, `last_login`, `created_at`, `updated_at`, `reset_token`, `reset_expires`, `profile_image`) VALUES
-(3, 'MUHAMMAD SHAMEEL', 'shameel681@gmail.com', '01110084626', 'no1. jalan semarak', '$2y$10$J149gfHywEFwTNTZStdDDeF/8bfLsEYp.uANEeycci5j.nXuqvMRG', 1, NULL, '2025-09-25 15:23:46', '2025-09-09 05:43:46', '2025-09-25 15:23:46', NULL, NULL, NULL),
-(5, 'Muhammad Fikri Bin Mawardi', 'toonpow3@gmail.com', '0119898256', 'AG-2, JALAN DESA KENANGA 2, TAMAN DESA KENANGA', '$2y$10$ByarGQwCoC2eLLtXq17pQuMLBrPpvXeLssb9QZVnmhEZ0QSFWSUdy', 1, NULL, '2025-09-25 14:55:04', '2025-09-09 06:32:48', '2025-09-25 14:55:04', NULL, NULL, 'capybara.jpg');
+(3, 'MUHAMMAD SHAMEEL', 'shameel681@gmail.com', '01110084626', 'no1. jalan semarak', '$2y$10$J149gfHywEFwTNTZStdDDeF/8bfLsEYp.uANEeycci5j.nXuqvMRG', 1, NULL, '2025-10-12 22:23:40', '2025-09-09 05:43:46', '2025-10-12 22:24:35', NULL, NULL, '3_1760279075.jpg'),
+(5, 'Muhammad Fikri Bin Mawardi', 'toonpow3@gmail.com', '0119898256', 'AG-2, JALAN DESA KENANGA 2, TAMAN DESA KENANGA', '$2y$10$ByarGQwCoC2eLLtXq17pQuMLBrPpvXeLssb9QZVnmhEZ0QSFWSUdy', 1, NULL, '2025-10-07 01:02:53', '2025-09-09 06:32:48', '2025-10-07 01:02:53', NULL, NULL, 'capybara.jpg');
 
 -- --------------------------------------------------------
 
@@ -84,8 +84,8 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`customer_id`, `name`, `email`, `password`, `phone_no`, `address`, `customer_image`, `reset_token`, `reset_expires`, `reset_expiry`, `is_verified`, `verify_token`, `profile_image`, `created_at`, `last_logged_in`, `updated_at`) VALUES
 (19, 'MUHAMMAD SUHAIMI BIN MOHD SHAM', 'm.suhaimipro@gmail.com', '$2y$10$i2Vv1tNp/a1nVGc61hnZbOoIRpasC0Z1mlPFQRTVdRQJ.Bw4QaD4K', '01162226128', 'AG-2, JALAN DESA KENANGA 2, TAMAN DESA KENANGA', NULL, NULL, NULL, NULL, 0, '91b93fa6d1fbbd6507b59041144081f4', NULL, '2025-09-19 13:32:35', NULL, '2025-09-25 07:01:01'),
 (20, 'MUHAMMAD ANAS IZZUDIN BIN MUAMAR', 'anasizzuddin@graduate.utm.my', '$2y$10$0CEJ4RszQYGHE60cw.YT1uQVYRXvHmx3vkN.eB8Cx0E9qlbaHpnzq', '0102045904', 'wangsa', NULL, NULL, NULL, NULL, 0, '4a37db85f42c739ce2adaa7d1489fcbd', NULL, '2025-09-19 13:32:35', NULL, '2025-09-25 07:02:29'),
-(33, 'MUHAMMAD SHAMEEL BIN SHAMSUL ADZMI', 'shameel681@gmail.com', '$2y$10$AexpywrEw4bh.ogj/TizROlEUHR/J.zKpTCSVdsPyqbX9sVqw1LtO', '01110084626', '', NULL, NULL, NULL, NULL, 1, NULL, NULL, '2025-09-19 13:32:35', '2025-09-25 07:18:00', '2025-09-25 07:18:00'),
-(37, 'FIKRI MAWARDI', 'toonpow43@gmail.com', '$2y$10$zOnBDUa/3aT8G4pEzCdbYOi6CVKpeQ2SUjEgUg7VrFMkO/a25zBiq', '0116222612', 'AG-2, JALAN DESA KENANGA 2, TAMAN DESA KENANGA', 'cust_37.jpg', NULL, NULL, NULL, 1, NULL, 'cust_37.jpg', '2025-09-22 22:59:57', '2025-09-30 07:36:50', '2025-09-30 07:36:50');
+(33, 'MUHAMMAD SHAMEEL BIN SHAMSUL ADZMI', 'shameel681@gmail.com', '$2y$10$AexpywrEw4bh.ogj/TizROlEUHR/J.zKpTCSVdsPyqbX9sVqw1LtO', '01110084626', 'buloh', 'cust_33.jpg', NULL, NULL, NULL, 1, NULL, NULL, '2025-09-19 13:32:35', '2025-10-12 14:20:51', '2025-10-12 14:20:51'),
+(37, 'FIKRI MAWARDI', 'toonpow43@gmail.com', '$2y$10$zOnBDUa/3aT8G4pEzCdbYOi6CVKpeQ2SUjEgUg7VrFMkO/a25zBiq', '0116222612', 'AG-2, JALAN DESA KENANGA 2, TAMAN DESA KENANGA', 'cust_37.jpg', NULL, NULL, NULL, 1, NULL, 'cust_37.jpg', '2025-09-22 22:59:57', '2025-10-01 07:21:58', '2025-10-01 07:21:58');
 
 -- --------------------------------------------------------
 
@@ -133,7 +133,8 @@ INSERT INTO `feedback_guest` (`id`, `guest_name`, `guest_email`, `feedback`, `cr
 (27, 'shameel clone', 'shameeldoubleganger@gmail.com', 'makanan ini tidak sedap', '2025-08-17 19:20:54'),
 (28, 'shameel', 'shameel@hotmail.com', 'makanan 10/10', '2025-08-17 19:23:15'),
 (29, 'MUHAMMAD ZAKUAN', 'mfitrizakuan@gmail.com', 'MAKANAN RASA BIASA BIASA JE', '2025-08-17 19:32:03'),
-(31, 'fitrizakuanazmee', 'm.fitreezakuanazmee@gmail.com', 'syedap', '2025-09-07 09:56:33');
+(31, 'fitrizakuanazmee', 'm.fitreezakuanazmee@gmail.com', 'syedap', '2025-09-07 09:56:33'),
+(33, 'Muhammad Fikri Bin Mawardi-', 'toonpow43@gmail.com', 'Sedap siot', '2025-09-30 18:15:29');
 
 -- --------------------------------------------------------
 
@@ -192,6 +193,37 @@ CREATE TRIGGER `before_insert_menu` BEFORE INSERT ON `menu` FOR EACH ROW BEGIN
 END
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `customer_name` varchar(255) DEFAULT NULL,
+  `order_date` datetime DEFAULT current_timestamp(),
+  `total_amount` decimal(10,2) NOT NULL,
+  `payment_status` enum('Pending','Paid','Cancelled') DEFAULT 'Pending',
+  `order_status` enum('Processing','Completed','Cancelled') DEFAULT 'Processing',
+  `receipt_sent` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `food_id` varchar(10) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price_each` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -259,6 +291,21 @@ ALTER TABLE `menu`
   ADD PRIMARY KEY (`food_id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `fk_orders_customer` (`customer_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`order_item_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `food_id` (`food_id`);
+
+--
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
@@ -291,13 +338,42 @@ ALTER TABLE `feedback_customer`
 -- AUTO_INCREMENT for table `feedback_guest`
 --
 ALTER TABLE `feedback_guest`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
   MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_orders_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`food_id`) REFERENCES `menu` (`food_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
